ff01d3299fd78f9bbf5bf0ed4c8a604241f245d1
Victor Hugo Freitas Magalhães <vmagalhaes@brex.com>
2023-08-05 17:58:18 -0300

    chore: Improve debugging

--- script.ts
+++ script.ts
@@ -1,8 +1,7 @@
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
-import readline from "node:readline/promises";
 import fs from "node:fs/promises";
-import { Page } from "puppeteer";
+import { Page, Frame } from "puppeteer";
 import Papa from "papaparse";
 import ms, { StringValue as MsString } from "ms";
 
@@ -10,16 +9,77 @@ puppeteer.use(StealthPlugin());
 
 const idList = new Set<string>();
 const idMap = new Map<string, Record<string, string>>();
-const duplicateCountMap = new Map<string, number>();
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const skippedPages: string[] = [];
 
 process.on("unhandledRejection", (reason, promise) => {
   console.warn("Unhandled Rejection at:", promise, "reason:", reason);
 });
+
+const originalConsole = console;
+Object.assign(globalThis, { originalConsole });
+
+const patchConsole = async (pageRef: WeakRef<Page>) => {
+  {
+    const page = pageRef.deref();
+    if (!page) return;
+
+    page.exposeFunction(
+      "callNodeConsole",
+      (methodName: keyof Console, ...args: any[]) => {
+        originalConsole.log("callNodeConsole", { methodName, args });
+        (originalConsole[methodName] as any)(...args);
+      }
+    );
+
+    page.evaluate(() => {
+      const originalConsole = globalThis.console;
+      Object.assign(globalThis, {
+        originalConsole,
+        console: new Proxy(originalConsole, {
+          get(target, prop, receiver) {
+            originalConsole.log("get", { target, prop, receiver });
+            const originalValue = Reflect.get(target, prop, receiver);
+            if (typeof originalValue === "function") {
+              return (...args: any[]) => {
+                (originalValue as any)(...args);
+                (globalThis as any).callNodeConsole(prop, ...args);
+              };
+            } else {
+              return originalValue;
+            }
+          },
+        }),
+      });
+    });
+  }
+
+  console = new Proxy(originalConsole, {
+    get(target, prop, receiver) {
+      const originalValue = Reflect.get(target, prop, receiver);
+      if (typeof originalValue === "function") {
+        return {
+          [prop]: (...args: any[]) => {
+            originalValue(...args);
+            const page = pageRef.deref();
+            if (page && !page.isClosed()) {
+              page.evaluate(
+                (methodName, args) => {
+                  const console =
+                    (globalThis as any).originalConsole ?? globalThis.console;
+                  console[methodName](...args);
+                },
+                prop,
+                args
+              );
+            }
+          },
+        }[prop as string];
+      } else {
+        return originalValue;
+      }
+    },
+  });
+};
 
 const goToNextPage = async (
   page: Page,
@@ -57,7 +117,20 @@ const goToNextPage = async (
   return hasNextPage;
 };
 
-const pauseNode = () => rl.question("Press enter to continue");
+async function detectAndHandleRecaptcha(frame: Frame) {
+  const page = frame.page();
+  if (frame.url().includes("recaptcha/")) {
+    console.log("Recaptcha detectado", frame.url());
+    await Promise.all([
+      page.bringToFront(),
+      page.evaluate(() =>
+        document.querySelector(`iframe[src*="recaptcha/"]`)?.scrollIntoView()
+      ),
+      page.evaluate(() => alert("Resolva o recaptcha")),
+    ]);
+  }
+}
+
 const pauseBrowser = (page: Page) =>
   page.evaluate(() => {
     debugger;
@@ -66,33 +139,25 @@ const pauseBrowser = (page: Page) =>
 const takeScreenshot = (page: Page) =>
   page.screenshot({ path: "output/screenshot.png" });
 
-let abortController: AbortController | null = null;
-
 const DEFAULT_TIMEOUT: MsString = "2 min";
 
 async function main() {
-  const browser = await puppeteer.launch({ headless: false, devtools: true });
-  browser.on("disconnected", () => rl.close());
+  const browser = await puppeteer.launch({
+    headless: false,
+    devtools: true,
+    args: ["--start-maximized"],
+  });
 
   const incognito = await browser.createIncognitoBrowserContext();
 
   const page = await incognito.newPage();
-  page.on("frameattached", async (frame) => {
-    if (frame.url().includes("recaptcha/")) {
-      console.log("recaptcha detectado");
-      await Promise.all([
-        page.bringToFront(),
-        page.evaluate(() =>
-          document.querySelector(`iframe[src*="recaptcha/"]`)?.scrollIntoView()
-        ),
-        page.evaluate(() => alert("Resolva o recaptcha")),
-      ]);
-    }
-  });
+  page.on("close", () => browser.close());
+
+  patchConsole(new WeakRef(page));
+  page.on("framenavigated", detectAndHandleRecaptcha);
 
   page.setDefaultTimeout(ms(DEFAULT_TIMEOUT));
 
-  await page.setViewport({ width: 1080, height: 1024 });
   await page.goto("https://portal.cfm.org.br/busca-medicos/");
 
   {
@@ -116,77 +181,77 @@ async function main() {
     page.setDefaultTimeout(ms(DEFAULT_TIMEOUT));
   }
 
+  const lastPageButtonSelector = ".paginationjs-page.paginationjs-last";
+  await page.waitForSelector(lastPageButtonSelector);
+  const totalPages = await page.evaluate(
+    (selector) => document.querySelector(selector)?.textContent,
+    lastPageButtonSelector
+  );
+  console.log(`Initiating extraction script. Total pages: ${totalPages}`);
+
   let pageNumber: string | undefined = "1";
   do {
-    const itemSelector = ".resultado-item";
-    await page.waitForSelector(itemSelector);
-    console.log("Items loaded, waiting for aditional info to load");
-    await page.waitForSelector(".row.endereco", { visible: true });
-    await page.waitForNetworkIdle();
-    console.log("Additional info loaded, extracting data");
-    const pageData = await page.evaluate((selector) => {
-      return [...document.querySelectorAll(selector)].map((item) => {
-        type Entry = [keyof any, unknown];
-        const staticEntries = [
-          ["Nome", item.querySelector("h4")!.textContent!.trim()],
-        ] satisfies Entry[];
-        return Object.fromEntries(
-          staticEntries.concat(
-            [...item.querySelectorAll("b")].map((b) => {
-              const [key, value] = [...b.parentElement!.childNodes]
-                .map((node) => node.textContent?.trim())
-                .filter(Boolean);
-              return [key!.replace(/:$/, ""), value!] satisfies Entry;
-            })
-          )
-        );
-      });
-    }, itemSelector);
+    console.group(`Page ${pageNumber}/${totalPages}`);
+    const dedupedItemCountBefore = idList.size;
+    try {
+      const itemSelector = ".resultado-item";
+      await page.waitForSelector(itemSelector);
+      console.log("Items loaded, waiting for aditional info to load");
+      await page.waitForSelector(".row.endereco", { visible: true });
+      await page.waitForNetworkIdle();
+      console.log("Additional info loaded, extracting data");
+      const pageData = await page.evaluate((selector) => {
+        return [...document.querySelectorAll(selector)].map((item) => {
+          type Entry = [keyof any, unknown];
+          const staticEntries = [
+            ["Nome", item.querySelector("h4")!.textContent!.trim()],
+          ] satisfies Entry[];
+          return Object.fromEntries(
+            staticEntries.concat(
+              [...item.querySelectorAll("b")].map((b) => {
+                const [key, value] = [...b.parentElement!.childNodes]
+                  .map((node) => node.textContent?.trim())
+                  .filter(Boolean);
+                return [key!.replace(/:$/, ""), value!] satisfies Entry;
+              })
+            )
+          );
+        });
+      }, itemSelector);
 
-    for (const item of pageData) {
-      const id = item["CRM"]!;
-      if (!idList.has(id)) {
-        idList.add(id);
-        idMap.set(id, { ...item, Página: pageNumber });
-      } else {
-        const count = duplicateCountMap.get(id) ?? 0;
-        duplicateCountMap.set(id, count + 1);
+      for (const item of pageData) {
+        const id = item["CRM"]!;
+        if (!idList.has(id)) {
+          idList.add(id);
+          idMap.set(id, { ...item, Página: pageNumber });
+        }
       }
-    }
 
-    abortController?.abort();
-    abortController = new AbortController();
+      if (idList.size === dedupedItemCountBefore) {
+        skippedPages.push(pageNumber);
+      }
 
-    const { signal } = abortController;
-    const timerHandle = setImmediate(async () => {
-      console.log("setImmediate called");
+      const timerLabel = "Report & write partial results";
+      console.time(timerLabel);
       console.log("Partial results", {
         pageNumber,
         total: idList.size,
         lastItem: idMap.get([...idList].slice(-1)[0]),
-        totalDuplicates: [...duplicateCountMap].reduce(
-          (acc, [_, v]) => acc + v,
-          0
-        ),
-        duplicatesPerId: Object.fromEntries(
-          [...duplicateCountMap].sort((a, b) => b[1] - a[1])
-        ),
+        skippedPages,
       });
 
       const data = [...idMap.values()];
       await Promise.all([
-        fs.writeFile("output/data.json", JSON.stringify(data), {
-          signal,
-        }),
         fs.writeFile(
-          "output/duplicates.json",
-          JSON.stringify([...duplicateCountMap.values()]),
-          { signal }
+          "output/data.json",
+          JSON.stringify({ data, skippedPages })
         ),
-        fs.writeFile("output/data.csv", Papa.unparse(data), { signal }),
+        fs.writeFile("output/data.csv", Papa.unparse(data)),
       ]);
-    });
-    signal.addEventListener("abort", () => clearImmediate(timerHandle));
+      console.timeEnd(timerLabel);
+    } finally {
+      console.groupEnd();
+    }
   } while (
     await goToNextPage(page, (n) => {
       pageNumber = n;
